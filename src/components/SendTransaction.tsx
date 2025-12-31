ec36bd1583bf87ab5145690996229df4972f0580
Ironor25 <rock.on0103@gmail.com>
2026-01-01 00:40:27 +0530

    completed send_transaction

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -3,24 +3,28 @@ import { useCallback, useState } from "react";
 import { Transaction,SystemProgram, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
 
 
+
 export function SendTransaction(){
     const {connection} = useConnection();
     const {publicKey,sendTransaction} = useWallet();
     const [enable, setEnable] = useState(false)
-    const [message, setMessage] = useState(String)
-    const [receiver_address , setReceiver_address ] = useState(String)
-    const [sol_amount , setSol_amount] = useState(String)
+    const [message, setMessage] = useState("")
+    const [receiver_address , setReceiver_address ] = useState("")
+    const [sol_amount , setSol_amount] = useState(Number)
     const handle_send_trxn = useCallback( async ()=> {
 
         try{
 
             const {blockhash,lastValidBlockHeight} =  await connection.getLatestBlockhash()
+            
             if (!publicKey || !receiver_address) throw new Error("Info not available!")
+            if (!sol_amount || isNaN(sol_amount ) || sol_amount===0) throw new Error("Invalid amount")
+                
             const trxn = new Transaction().add(
                             SystemProgram.transfer({
                                     fromPubkey:publicKey,
                                     toPubkey:new PublicKey(receiver_address),
-                                    lamports: Number(sol_amount)* LAMPORTS_PER_SOL,
+                                    lamports: sol_amount* LAMPORTS_PER_SOL,
                             }))
 
             const signature = await sendTransaction(trxn,connection)
@@ -33,15 +37,17 @@ export function SendTransaction(){
         catch{
             throw new Error("Transaction failed X")
         }
-    },[publicKey])
+    },[publicKey,receiver_address,sol_amount,connection,sendTransaction])
     
 
 
     return(
-        <div className="p-4 flex">
+        <div className="p-4 flex-col">
+            <div className="flex">
             <div className="flex flex-col">
             <span>Address</span>
-            <input type="text" 
+            <input type="text"
+            value={receiver_address} 
             className="border-2 rounded-2xl p-2 mr-2"
             onChange={(e)=>{
                 setReceiver_address(e.target.value)
@@ -49,8 +55,10 @@ export function SendTransaction(){
             </div>
             <div className="flex flex-col">
             <span>Amount</span>
-            <input type="text" 
-            className="border-2 rounded-2xl p-2 mr-2"
+            <input type="number"
+            
+            value={sol_amount} 
+            className="border-2 rounded-2xl p-2 mr-2 scroll-m-0"
             onChange={(e)=>{
                 setSol_amount(e.target.value)
             }} />
@@ -59,8 +67,9 @@ export function SendTransaction(){
             className="text-white bg-purple-400 rounded-2xl  mt-6 hover:bg-purple-500 cursor-pointer w-30">
             Send
             </button>
+            </div>
             <div>
-                {enable?(<span>Verified Successfuly , Message Signature: ${message}</span>):(<span></span>)}
+                {enable?(<span>{sol_amount} {message}</span>):(<span></span>)}
             </div>
             
         </div>
